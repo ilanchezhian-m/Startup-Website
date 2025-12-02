2652c2e26de47d6c0ec01e40da881abe7c9002f7
ilanchezhian-m <ilanchezhian27@gmail.com>
2025-12-02 11:19:20 +0530

    udpdate header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -26,8 +26,8 @@ export function Header() {
   };
 
   return (
-    <div className="container">
-      <header className="flex ">
+    <div className="">
+      <header className=" ">
         <div className="bg-black w-full text-white px-8 py-7">
           <div className="flex items-center justify-between pr-5  ">
             <h5 className="text-xs sm:text-xl mb-4 md:mb-0 md:mr-4">
